f7856c30d511c5b05e6667578fd200734b7e38d3
thienkq <64583022+thienkq@users.noreply.github.com>
2022-05-31 09:31:37 +0700

    init resume page

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,57 @@
 import Head from 'next/head'
+import { Profiler } from 'react/cjs/react.production.min'
 
 export default function Home() {
+  const resumeData = {
+    "basics": {
+      "name": "Kieu Quang Thien",
+      "label": "Engineering Things",
+      "image": "",
+      "email": "thienkq@gmail.com",
+      "phone": "0934.433.505",
+      "url": "https://github.com",
+      "summary": "A summary of Thien…",
+      "location": {
+        "address": "2712 Broadway St",
+        "postalCode": "CA 94115",
+        "city": "San Francisco",
+        "countryCode": "US",
+        "region": "California"
+      },
+      "profiles": [{
+        "network": "Linkedin",
+        "username": "thienkq",
+        "url": "https://www.linkedin.com/in/thien-kieu-quang-22860534/"
+      }]
+    },
+    "work": [{
+      "name": "Company",
+      "position": "President",
+      "url": "https://company.com",
+      "startDate": "2013-01-01",
+      "endDate": "2014-01-01",
+      "summary": "Description…",
+      "highlights": [
+        "Started the company"
+      ]
+    }]
+  }
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Create Resume</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          {resumeData.basics.name}
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <code>{resumeData.basics.label}</code>
         </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer>
