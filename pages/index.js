2196d16dc738a4be678033fdcfa6bfb32df7ab67
thienkq <64583022+thienkq@users.noreply.github.com>
2022-05-31 10:14:47 +0700

    add some data

--- pages/index.js
+++ pages/index.js
@@ -1,215 +1,81 @@
-import Head from 'next/head'
-import { Profiler } from 'react/cjs/react.production.min'
+import Head from 'next/head';
 
 export default function Home() {
-  const resumeData = {
-    "basics": {
-      "name": "Kieu Quang Thien",
-      "label": "Engineering Things",
-      "image": "",
-      "email": "thienkq@gmail.com",
-      "phone": "0934.433.505",
-      "url": "https://github.com",
-      "summary": "A summary of Thien…",
-      "location": {
-        "address": "2712 Broadway St",
-        "postalCode": "CA 94115",
-        "city": "San Francisco",
-        "countryCode": "US",
-        "region": "California"
-      },
-      "profiles": [{
-        "network": "Linkedin",
-        "username": "thienkq",
-        "url": "https://www.linkedin.com/in/thien-kieu-quang-22860534/"
-      }]
-    },
-    "work": [{
-      "name": "Company",
-      "position": "President",
-      "url": "https://company.com",
-      "startDate": "2013-01-01",
-      "endDate": "2014-01-01",
-      "summary": "Description…",
-      "highlights": [
-        "Started the company"
-      ]
-    }]
-  }
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Resume</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	const resumeData = {
+		basics: {
+			name: 'Kieu Quang Thien',
+			label: 'Engineering Things',
+			image: '',
+			email: 'thienkq@gmail.com',
+			phone: '0934.433.505',
+			url: 'https://github.com',
+			summary: 'A summary of Thien…',
+			location: {
+				address: '2712 Broadway St',
+				postalCode: 'CA 94115',
+				city: 'San Francisco',
+				countryCode: 'US',
+				region: 'California',
+			},
+			profiles: [
+				{
+					network: 'Linkedin',
+					username: 'thienkq',
+					url: 'https://www.linkedin.com/in/thien-kieu-quang-22860534/',
+				},
+			],
+		},
+		work: [
+			{
+				name: 'Company',
+				position: 'President',
+				url: 'https://company.com',
+				startDate: '2013-01-01',
+				endDate: '2014-01-01',
+				summary: 'Description…',
+				highlights: ['Started the company'],
+			},
+		],
+	};
+	return (
+		<div className="container">
+			<Head>
+				<title>Resume</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <main>
-        <h1 className="title">
-          {resumeData.basics.name}
-        </h1>
-
-        <p className="description">
-          <code>{resumeData.basics.label}</code>
-        </p>
-
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+			<header id="header">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-9 col-sm-push-3">
+							<h1>{resumeData.basics.name}</h1>
+							<h2>{resumeData.basics.label}</h2>
+						</div>
+					</div>
+				</div>
+			</header>
+			<div id="content" class="container">
+				<section id="contact" class="row">
+					<aside class="col-sm-3">
+						<h3>Contact</h3>
+					</aside>
+					<div class="col-sm-9">
+						<div class="row">
+							<div class="col-sm-6">
+								<strong>Email</strong>
+								<div class="email">{resumeData.basics.email}</div>
+							</div>
+							<div class="col-sm-6">
+								<strong>Website</strong>
+								<div class="website">
+									<a href="">{resumeData.basics.url}</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
+      
+		</div>
+	);
 }
